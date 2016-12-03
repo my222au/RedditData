a2037b7be1dd9505abf7b967fbfb9734aa76b037
MohamedOsman <my222au@student.lnu.se>
2016-12-03 10:24:43 +0100

    Project Started

---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+
+/**
+ * Todo:  Setting up the Reddit database
+ * Todo 2: automate as much as you can,such as setting up tables, importing data, etc.
+ * { KEYS
+ "parent_id":"t1_co779e1","id":"co77k46",
+ "score_hidden":false, "gilded":0,
+ "author_flair_text":null,"author_flair_css_class":null,
+ "name":"t1_co77k46","body":"will keep you posted!",
+ "created_utc":"1422748973","downs":0,
+ "ups":-1,"distinguished":null,
+ "controversiality":0,"link_id":"t3_2ucrbf",
+ "subreddit_id":"t5_2uqch","score":-1,
+ "archived":false,"subreddit":"AskNYC",
+ "edited":false,"retrieved_on":1424281729,
+ "author":"adbac36"
+ }
+  */
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
