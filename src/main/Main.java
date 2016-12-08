6da461ad325eb4bbe1589cb03c0104ca706dbdaa
Mohamed Osman <my222au@student.lnu.se>
2016-12-08 08:22:30 +0100

    added TODO

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,20 +2,10 @@ package main;
 
 
 /**
- * Todo:  Setting up the Reddit database
- * Todo 2: automate as much as you can,such as setting up tables, importing data, etc.
- * { KEYS
- "parent_id":"t1_co779e1","id":"co77k46",
- "score_hidden":false, "gilded":0,
- "author_flair_text":null,"author_flair_css_class":null,
- "name":"t1_co77k46","body":"will keep you posted!",
- "created_utc":"1422748973","downs":0,
- "ups":-1,"distinguished":null,
- "controversiality":0,"link_id":"t3_2ucrbf",
- "subreddit_id":"t5_2uqch","score":-1,
- "archived":false,"subreddit":"AskNYC",
- "edited":false,"retrieved_on":1424281729,
- "author":"adbac36"
+ * .
+ * TODO 1  Import and parse  the  json file
+ * TODO 2  set up att database and then covert the json file to
+ * TODO 3 Create a E/R MODEL
  }
   */
 
